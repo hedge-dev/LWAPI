a5cebfe249f9a886242588a8d6e45e9e100a441b
Sajid <sajidur78@gmail.com>
2022-01-06 19:16:12 +0600

    SceneParameters::GetSceneData

--- Sonic2013/System/Services/service_SceneParameters.h
+++ Sonic2013/System/Services/service_SceneParameters.h
@@ -45,6 +45,11 @@ namespace app::xgame
 				m_Backups[0] = m_Backups[1];
 		}
 
+		const FxSceneData* GetSceneData() const 
+		{
+			return m_pSceneData;
+		}
+
 		FxSceneData* GetEditingData()
 		{
 			if (!m_Backups.capacity())
