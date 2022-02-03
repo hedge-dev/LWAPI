24ef418354075d1c9b413c79918bdc5cf7c63ac9
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2022-02-03 14:43:21 +0100

    Add more ObjUtil functions

--- Sonic2013/System/Utility/ObjUtil.h
+++ Sonic2013/System/Utility/ObjUtil.h
@@ -5,14 +5,45 @@ namespace app
 	class ObjUtil
 	{
 	public:
+		template <class T>
+		static T* GetObjectInfo(GameDocument& rDocument, const char* pName)
+		{
+			return reinterpret_cast<T*>(rDocument.GetService<CObjInfoContainer>()->GetInfo(pName));
+		}
+
+		static const char* GetStagePackName(GameDocument& rDocument)
+		{
+			static csl::ut::FixedString<32> buffer{};
+
+			auto* pLvlInfo = rDocument.GetService<CLevelInfo>();
+			buffer.setf("%s_obj.pac", pLvlInfo->m_Level);
+
+			return buffer;
+		}
+
 		static hh::ut::PackFile GetPackFile(const char* pName)
 		{
 			const auto data = csl::fnd::Singleton<fnd::ResourceManager>::GetInstance()->Get<fnd::ResRawData>(pName);
 			if (!data.IsValid())
-				return hh::ut::PackFile(nullptr);
+				return { nullptr };
 
-			const auto pack = hh::ut::PackFile(data->GetAddress());
-			return hh::ut::PackFile(pack);
+			return hh::ut::PackFile(data->GetAddress());
+		}
+
+		static hh::gfx::res::ResModel GetModelResource(const char* pName, hh::ut::PackFile& rPackFile)
+		{
+			if (!rPackFile.IsValid())
+				return { nullptr };
+
+			return rPackFile.Get<hh::gfx::res::ResModel>(pName);
+		}
+
+		static hh::gfx::res::ResSkeleton GetSkeletonResource(const char* pName, hh::ut::PackFile& rPackFile)
+		{
+			if (!rPackFile.IsValid())
+				return { nullptr };
+
+			return rPackFile.Get<hh::gfx::res::ResSkeleton>(pName);
 		}
 
 		static void SendMessageImmToSetObject(const GameObject& rObj, const CSetObjectID& id, fnd::Message& rMsg, bool create)
