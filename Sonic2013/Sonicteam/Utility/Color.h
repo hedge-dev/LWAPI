d68597a6513996d7007388fda521a7518a711877
Sajidur78 <sajidur78@gmail.com>
2022-04-10 21:00:22 +0600

    Add Colors

--- Sonic2013/Sonicteam/Utility/Color.h
+++ Sonic2013/Sonicteam/Utility/Color.h
@@ -9,4 +9,20 @@ namespace csl::ut
 		uint8 b;
 		uint8 a;
 	};
+
+#define DEFINE_COLOR(NAME, R, G, B, A) inline static constexpr Color8 NAME{R, G, B, A};
+
+	struct Colors
+	{
+	public:
+		DEFINE_COLOR(Transparent, 0, 0, 0, 0);
+		DEFINE_COLOR(White, 255, 255, 255, 255);
+		DEFINE_COLOR(Black, 0, 0, 0, 255);
+		DEFINE_COLOR(Red, 255, 0, 0, 255);
+		DEFINE_COLOR(Green, 0, 255, 0, 255);
+		DEFINE_COLOR(Blue, 0, 0, 255, 255);
+	};
+	using Colours = Colors;
+
+#undef DEFINE_COLOR
 }
