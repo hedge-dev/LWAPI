a28b508bfd48afb5f524c9fee2b433878d6854c1
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-17 16:23:11 +0200

    Recreate ObjDroppedRing

---
+++ Sonic2013/Object/Stage/Common/Ring/ObjDroppedRing.h
@@ -0,0 +1,418 @@
+#pragma once
+
+namespace app
+{
+	class CSharedObjInfo;
+
+	class CObjDroppedRing : public GameObject3D
+	{
+	public:
+		inline static constexpr int FLAG_DROPPED_BY_PLAYER = 2;
+		inline static constexpr int FLAG_IS_YOSHI_LEVEL = 5;
+
+	private:
+		inline static const char* ms_pRingEffectName = "ef_ob_com_yh1_ringget";
+		inline static const char* ms_pYoshiCoinEffectName = "ef_dl2_coin_get";
+		inline static const char* ms_pRingSoundName = "obj_ring";
+		inline static const char* ms_pYoshiCoinSoundName = "obj_yossycoin_get";
+		inline static const char* ms_pRingScoreName = "GET_RING";
+		inline static const char* ms_pDroppedRingScoreName = "GET_DROPPED_RING";
+		inline static const char* ms_pYoshiCoinScoreName = "GET_RING";
+		inline static const char* ms_pDroppedYoshiScoreName = "GET_DROPPED_RING";
+		inline static const size_t ms_ShapeCount = 1;
+		inline static const float ms_CollisionRadius = 3.0f;
+		inline static const float ms_ShadowRadius = 3.0f;
+		
+	public:
+		struct SUniqueInfo
+		{
+			inline static constexpr int FLAG_IS_2D = 0;
+			inline static constexpr int FLAG_DROPPED_BY_PLAYER = 2;
+			inline static constexpr int FLAG_IS_YOSHI_LEVEL = 5;
+
+			csl::math::Vector3 Position{};
+			csl::math::Vector3 Unk1{};
+			float Unk2{};
+			float Unk3{};
+			float LifeSpan{};
+			int Counter{};
+			game::PathEvaluator PathEvaluator{};
+			csl::ut::Bitset<uint32> Flags{};
+		};
+
+		const SUniqueInfo* pUniqueInfo{};
+		fnd::GOCVisualModel* pVisualModel{};
+		game::GOCEffect* pEffect{};
+		game::GOCCollider* pCollider{};
+		game::GOCGravity* pGravity{};
+		game::GOCMovementComplex* pMovementComplex{};
+		game::MoveBound* pMoveBound{};
+		Gimmick::CRingManager* pRingManager{};
+		float ElapsedTime{};
+		float LifeSpan{ 1.0f };
+		csl::ut::Bitset<uint32> Flags{};
+		char Counter{};
+		
+		CObjDroppedRing(const SUniqueInfo& in_rUniqueInfo) : GameObject3D()
+		{
+			pUniqueInfo = &in_rUniqueInfo;
+
+			GetComponent<fnd::GOCTransform>()->SetLocalTranslation(in_rUniqueInfo.Position);
+			AddProperty(0x4002, 2);
+		}
+
+	public:
+		void AddCallback(GameDocument& in_rDocument) override
+		{
+			fnd::GOComponent::Create<fnd::GOCVisualModel>(*this);
+			fnd::GOComponent::Create<game::GOCEffect>(*this);
+			fnd::GOComponent::Create<game::GOCCollider>(*this);
+			fnd::GOComponent::Create<game::GOCGravity>(*this);
+			fnd::GOComponent::Create<game::GOCShadowSimple>(*this);
+			fnd::GOComponent::Create<game::GOCSound>(*this);
+			fnd::GOComponent::Create<game::GOCMovementComplex>(*this);
+
+			fnd::GOComponent::BeginSetup(*this);
+
+			Flags.set(FLAG_IS_YOSHI_LEVEL, pUniqueInfo->Flags.test(SUniqueInfo::FLAG_IS_YOSHI_LEVEL));
+
+			if (pVisualModel = GetComponent<fnd::GOCVisualModel>())
+			{
+				fnd::GOCVisualModel::Description description{};
+				if (Flags.test(FLAG_IS_YOSHI_LEVEL))
+				{
+					if (auto* pInfo = ObjUtil::GetObjectInfo<ObjYoshiCoinInfo>(in_rDocument))
+						description.m_Model = pInfo->Model;
+				}
+				else
+				{
+					if (auto* pInfo = ObjUtil::GetObjectInfo<CSharedObjInfo>(in_rDocument))
+						description.m_Model = pInfo->RingModel;
+				}
+
+				pVisualModel->Setup(description);
+			}
+
+			if (GetComponent<game::GOCEffect>()) game::GOCEffect::SimpleSetup(this, 0, false);
+
+			if (pCollider = GetComponent<game::GOCCollider>())
+			{
+				pCollider->Setup({ ms_ShapeCount });
+				game::ColliSphereShapeCInfo collisionInfo{};
+				collisionInfo.m_ShapeType = game::CollisionShapeType::ShapeType::ShapeType_Sphere;
+				collisionInfo.m_MotionType = game::PhysicsMotionType::MotionType::MotionType_VALUE2;
+				collisionInfo.m_Unk2 = 1;
+				collisionInfo.m_ShapeID = 0;
+				collisionInfo.m_Radius = ms_CollisionRadius;
+				ObjUtil::SetupCollisionFilter(ObjUtil::eFilter_Unk12, collisionInfo);
+
+				auto pShape = pCollider->CreateShape(collisionInfo);
+				ObjUtil::SetEnableColliShape(pShape, false);
+			}
+
+			if (pGravity = GetComponent<game::GOCGravity>())
+			{
+				game::GOCGravity::Description description{};
+				if (pUniqueInfo->Flags.test(4))
+				{
+					description.Unk4 = true;
+				}
+				else
+				{
+					description.Type = 1;
+					description.Unk2 = 10.0f;
+				}
+
+				pGravity->Setup(description);
+			}
+
+			if (auto* pShadowSimple = GetComponent<game::GOCShadowSimple>())
+			{
+				game::ShadowHemisphereShapeCInfo shadowInfo{ ms_ShadowRadius };
+				shadowInfo.Unk2 = 5;
+				shadowInfo.ShadowQuality = 2;
+
+				game::GOCShadowSimple::Description description{ &shadowInfo };
+				pShadowSimple->Setup(description);
+			}
+
+			if (pEffect = GetComponent<game::GOCEffect>())
+				game::GOCEffect::SimpleSetup(this, 0, 0);
+
+			game::GOCSound::SimpleSetup(this, 0, 0);
+
+			if (pMovementComplex = GetComponent<game::GOCMovementComplex>())
+			{
+				if (pMoveBound = pMovementComplex->SetMoveController<game::MoveBound>())
+				{
+					game::MoveBound::Desc description{};
+					description.Velocity = pUniqueInfo->Unk1;
+					
+					if (pUniqueInfo->PathEvaluator.Component.IsValid())
+					{
+						description.PathEvaluator.SetPathObject(pUniqueInfo->PathEvaluator.GetPathObject());
+						description.PathEvaluator.SetDistance(pUniqueInfo->PathEvaluator.Distance);
+					}
+
+					if (pUniqueInfo->Flags.test(SUniqueInfo::FLAG_IS_2D))
+						description.Unk3 = 240.0f;
+					else
+						description.Unk3 = 350.0f;
+					
+					description.Unk4 = pUniqueInfo->Unk2;
+					description.Unk5 = pUniqueInfo->Unk3;
+					description.Unk6 = 167.332f;
+					description.Unk7 = pUniqueInfo->Counter;
+					description.Unk8 = 3;
+
+					if (pUniqueInfo->Flags.test(SUniqueInfo::FLAG_DROPPED_BY_PLAYER))
+					{
+						description.Flags.set(1);
+						Flags.set(FLAG_DROPPED_BY_PLAYER);
+					}
+					
+					if (pUniqueInfo->Flags.test(1))
+					{
+						description.Flags.set(2);
+						pMoveBound->RegisterSearchFunction(&app::game::SerachPlaneAndGrindPath);
+					}
+					
+					pMoveBound->Setup(description);
+				}
+			}
+			
+			fnd::GOComponent::EndSetup(*this);
+
+			pRingManager = in_rDocument.GetService<Gimmick::CRingManager>();
+			LifeSpan = pUniqueInfo->LifeSpan;
+
+			pUniqueInfo = NULL;
+		}
+
+		bool ProcessMessage(fnd::Message& in_rMessage) override
+		{
+			if (PreProcessMessage(in_rMessage))
+				return true;
+
+			switch (in_rMessage.GetType())
+			{
+			case xgame::MsgBeltConveyorControlEnd::MessageID:			return ProcMsgBeltConveyorControlEnd(static_cast<xgame::MsgBeltConveyorControlEnd&>(in_rMessage));
+			case xgame::MsgBeltConveyorControlStart::MessageID:			return ProcMsgBeltConveyorControlStart(static_cast<xgame::MsgBeltConveyorControlStart&>(in_rMessage));
+			case xgame::MsgHitEventCollision::MessageID:				return ProcMsgHitEventCollision(static_cast<xgame::MsgHitEventCollision&>(in_rMessage));
+			case xgame::MsgPressedBySnowBall::MessageID:				return ProcMsgPressedBySnowBall(static_cast<xgame::MsgPressedBySnowBall&>(in_rMessage));
+			case xgame::MsgRcGetHomingTargetInfo::MessageID:			return ProcMsgRcGetHomingTargetInfo(static_cast<xgame::MsgRcGetHomingTargetInfo&>(in_rMessage));
+			case xgame::MsgTornadoControlAction::MessageID:				return ProcMsgTornadoControlAction(static_cast<xgame::MsgTornadoControlAction&>(in_rMessage));
+			case xgame::MsgTornadoControlEnd::MessageID:				return ProcMsgTornadoControlEnd(static_cast<xgame::MsgTornadoControlEnd&>(in_rMessage));
+			case xgame::MsgTornadoControlStart::MessageID:				return ProcMsgTornadoControlStart(static_cast<xgame::MsgTornadoControlStart&>(in_rMessage));
+			default:													return GameObject3D::ProcessMessage(in_rMessage);
+			}
+		}
+
+		bool ProcMsgBeltConveyorControlEnd(xgame::MsgBeltConveyorControlEnd& in_rMessage)
+		{
+			pMovementComplex->EnableMovementFlag(0);
+			return true;
+		}
+
+		bool ProcMsgBeltConveyorControlStart(xgame::MsgBeltConveyorControlStart& in_rMessage)
+		{
+			if (Flags.test(8))
+			{
+				pMovementComplex->DisableMovementFlag(0);
+				in_rMessage.Unk1 = true;
+			}
+			else
+			{
+				in_rMessage.Unk1 = false;
+			}
+
+			in_rMessage.Unk5 = 3.0f;
+			in_rMessage.Unk6 = this;
+			in_rMessage.Unk3 = { 0.0f, 0.0f, 0.0f };
+			return true;
+		}
+
+		bool ProcMsgHitEventCollision(xgame::MsgHitEventCollision& in_rMessage)
+		{
+			if (Flags.test(6))
+				return true;
+
+			xgame::MsgTakeObject msg{ xgame::MsgTakeObject::EType::eType_DroppedRing };
+			msg.SetShapeUserID(in_rMessage.m_pOther->m_ID);
+			if (!SendMessageImm(in_rMessage.m_Sender, msg) || !msg.m_Taken)
+				return true;
+
+			const char* pEffectName = !Flags.test(5) ? ms_pRingEffectName : ms_pYoshiCoinEffectName;
+			if (pEffect) pEffect->CreateEffect(pEffectName);
+
+			const char* pSoundName = !Flags.test(5) ? ms_pRingSoundName : ms_pYoshiCoinSoundName;
+			if (auto* pSound = GetComponent<game::GOCSound>()) pSound->Play(pSoundName, 0.0f);
+			
+			const char* pScoreName = !Flags.test(5) ? ms_pDroppedRingScoreName : ms_pDroppedYoshiScoreName;
+			if (!Flags.test(2))
+				pScoreName = !Flags.test(5) ? ms_pRingScoreName : ms_pYoshiCoinScoreName;
+
+			int playerNo = ObjUtil::GetPlayerNo(*GetDocument(), in_rMessage.m_Sender);
+			ObjUtil::AddScorePlayerAction(*this, pScoreName, playerNo);
+			Flags.set(6);
+			Kill();
+
+			return true;
+		}
+
+		bool ProcMsgPressedBySnowBall(xgame::MsgPressedBySnowBall& in_rMessage)
+		{
+			if (!in_rMessage.pSnowBall)
+				return true;
+
+			if (auto* pVisualModel = GetComponent<fnd::GOCVisualModel>())
+			{
+				StickerObjectCInfoBase createInfo{};
+				createInfo.Unk1 = in_rMessage.Mtx;
+				pVisualModel->GetMatrix(&createInfo.Unk2);
+				createInfo.pParentObject = in_rMessage.pSnowBall;
+
+				if (obj_sticker::CreateStickerObject(GetAllocator(), *GetDocument(), createInfo))
+					Kill();
+			}
+
+			return true;
+		}
+
+		bool ProcMsgRcGetHomingTargetInfo(xgame::MsgRcGetHomingTargetInfo& in_rMessage)
+		{
+			in_rMessage.Unk1 = GetComponent<fnd::GOCTransform>()->GetLocalPosition();
+			in_rMessage.Unk2.reset();
+			return true;
+		}
+
+		bool ProcMsgTornadoControlAction(xgame::MsgTornadoControlAction& in_rMessage)
+		{
+			pCollider->SetEnable(false);
+			return true;
+		}
+
+		bool ProcMsgTornadoControlEnd(xgame::MsgTornadoControlEnd& in_rMessage)
+		{
+			Kill();
+			return true;
+		}
+
+		bool ProcMsgTornadoControlStart(xgame::MsgTornadoControlStart& in_rMessage)
+		{
+			in_rMessage.Unk1 = this;
+			in_rMessage.Unk2 = 0;
+			in_rMessage.Unk3 = 0;
+			pMovementComplex->DisableMovementFlag(0);
+			return true;
+		}
+
+		void Update(const fnd::SUpdateInfo& in_rUpdateInfo) override
+		{
+			ElapsedTime += in_rUpdateInfo.deltaTime;
+			if (ElapsedTime >= LifeSpan * 5.0f)
+			{
+				Kill();
+				return;
+			}
+
+			if (Flags.test(8))
+			{
+				if (++Counter >= 3)
+				{
+					Counter = 0;
+					
+					auto* pTransform = GetComponent<fnd::GOCTransform>();
+					csl::math::Vector3 yAxis{ pTransform->m_Transform.GetTransformMatrix().GetColumn(1) };
+					csl::math::Vector3 rayStartPos{ pTransform->GetLocalPosition() };
+					csl::math::Vector3 rayEndPos{ pTransform->GetLocalPosition() - (yAxis * 3.1f)};
+
+					if (ObjUtil::RaycastHitCollision(*GetDocument(), rayStartPos, rayEndPos, 0xC992))
+					{
+						pMoveBound->SetFlag(game::MoveController::FlagType::eFlagType_1);
+						Flags.reset(8);
+					}
+				}
+
+				UpdateResources(in_rUpdateInfo);
+				return;
+			}
+
+			if (!pMoveBound->Flags.test(0))
+			{
+				UpdateResources(in_rUpdateInfo);
+				return;
+			}
+
+			auto* pContextParam = pMovementComplex->GetContextParam();
+			auto gravityDir = pGravity->GetGravityDirection();
+			float power = pContextParam->Velocity.dot(gravityDir);
+			
+			if (fabs(power) * (1.0f / csl::math::Max(pMoveBound->Plane.Normal.dot(-gravityDir), 0.1f)) > 20.0f)
+			{
+				UpdateResources(in_rUpdateInfo);
+				return;
+			}
+
+			pMoveBound->ResetFlag(game::MoveController::FlagType::eFlagType_1);
+			Flags.set(8);
+			UpdateResources(in_rUpdateInfo);
+		}
+
+		void UpdateResources(const fnd::SUpdateInfo& in_rUpdateInfo)
+		{
+			if (!Flags.test(7))
+				JudgeEnableCollision();
+		
+			BlinkRing(in_rUpdateInfo.frame);
+			UpdateModelRotation();
+		}
+
+		void JudgeEnableCollision()
+		{
+			if (Flags.test(2))
+			{
+				if (ElapsedTime >= 0.8f && pMoveBound->Flags.test(0))
+					Flags.set(7);
+			}
+			else if (pMoveBound->Flags.test(0) || ElapsedTime >= 0.8f)
+			{
+				Flags.set(7);
+			}
+
+			if (!Flags.test(7) || !pCollider)
+				return;
+		
+			ObjUtil::SetEnableColliShape(pCollider, 0, true);
+		}
+
+		void BlinkRing(uint in_frame)
+		{
+			if (!pVisualModel)
+				return;
+		
+			if (ElapsedTime >= -((2.5f * LifeSpan) - (5.0f * LifeSpan)) &&
+				(ElapsedTime >= (5.0f * LifeSpan - 1.0f) || (in_frame & 1) == 0))
+			{
+				pVisualModel->SetVisible(!pVisualModel->IsVisible());
+			}
+		}
+
+		void UpdateModelRotation()
+		{
+			if (!pRingManager)
+				return;
+
+			auto gravityDir = pGravity->GetGravityDirection();
+			csl::math::Quaternion rotNorm{};
+			csl::math::QuaternionRotationBetweenNormals(&rotNorm, { 0.0f, -1.0f, 0.0f }, gravityDir);
+
+			csl::math::Quaternion rotAngle(Eigen::AngleAxisf(pRingManager->Unk2, csl::math::Vector3::UnitY()));
+
+			csl::math::Quaternion rotation(rotNorm * rotAngle);
+			rotation.normalize();
+
+			GetComponent<fnd::GOCTransform>()->SetLocalRotation(rotation);
+		}
+	};
+}
---
+++ Sonic2013/Object/Stage/Common/Ring/ObjDroppedRingCreate.h
@@ -0,0 +1,146 @@
+#pragma once
+
+namespace app
+{
+	struct SDroppedRingInfo
+	{
+		csl::math::Vector3 Position{};
+		int RingCount{};
+		bool DroppedByPlayer{};
+		bool Is2D{};
+		char Unk5{};
+		float Unk6{ 1.0f };
+		float LifeSpan{ 1.0f };
+	};
+}
+
+namespace app::dropped_ring
+{
+	inline void Create(GameDocument& in_rDocument, const SDroppedRingInfo& in_rInfo)
+	{
+		CObjDroppedRing::SUniqueInfo uniqueInfo{};
+		csl::math::Vector3 gravityDir{ 0.0f, -1.0f, 0.0f };
+		csl::math::Quaternion rotation{ csl::math::Quaternion::Identity };
+
+		auto* pLevelInfo = in_rDocument.GetService<CLevelInfo>();
+		bool isYoshiLevel = strcmp(pLevelInfo->m_Level, "zdlc02") == 0;
+		if (isYoshiLevel)
+		{
+			ObjUtil::PlaySE2D("obj_yossycoin_lost", { HH_SOUND_DEVICE_AUTO });
+		}
+		else if (in_rInfo.DroppedByPlayer)
+		{
+			ObjUtil::PlaySE2D("obj_ringspread", { HH_SOUND_DEVICE_AUTO });
+		}
+		else
+		{
+			ObjUtil::PlaySE2D("obj_ringspread2", { HH_SOUND_DEVICE_AUTO });
+		}
+
+		if (!in_rInfo.Unk5)
+		{
+			if (auto* pGravityManager = in_rDocument.GetService<game::GravityManager>())
+			{
+				if (auto* pGravityField = pGravityManager->GetObjectAtPoint(in_rInfo.Position))
+				{
+					if (pGravityField->GetDirection(in_rInfo.Position, &gravityDir))
+					{
+						csl::math::QuaternionRotationBetweenNormals(&rotation, { 0.0f, -1.0f, -0.0f }, gravityDir);
+					}
+				}
+			}
+		}
+
+		uniqueInfo.Counter = 0;
+		if (in_rInfo.Is2D)
+		{
+			game::PathRaycastInput rayInput{};
+			rayInput.From = csl::math::Vector3(in_rInfo.Position - gravityDir * 3.0f);
+			rayInput.To = csl::math::Vector3(in_rInfo.Position + gravityDir * 400.0f);
+			rayInput.Unk1 = 1;
+			rayInput.Unk2 = 1;
+
+			game::PathRaycastOutput rayOutput{};
+
+			if (auto* pPathManager = in_rDocument.GetService<game::PathManager>())
+			{
+				if (pPathManager->CastRay(rayInput, &rayOutput))
+				{
+					uniqueInfo.PathEvaluator.SetPathObject(rayOutput.pComponent);
+					uniqueInfo.PathEvaluator.SetDistance(rayOutput.Distance);
+					uniqueInfo.Flags.set(CObjDroppedRing::SUniqueInfo::FLAG_IS_2D, true);
+				}
+			}
+		}
+
+		uniqueInfo.Position = in_rInfo.Position;;
+		uniqueInfo.Flags.set(CObjDroppedRing::SUniqueInfo::FLAG_DROPPED_BY_PLAYER, in_rInfo.DroppedByPlayer);
+		uniqueInfo.Flags.set(4, in_rInfo.Unk5);
+		uniqueInfo.Flags.set(CObjDroppedRing::SUniqueInfo::FLAG_IS_YOSHI_LEVEL, isYoshiLevel);
+		uniqueInfo.Unk2 = 0.8f;
+		uniqueInfo.Unk3 = 0.75f;
+		uniqueInfo.LifeSpan = in_rInfo.LifeSpan;
+
+		if (!in_rInfo.RingCount)
+			return;
+
+		if (uniqueInfo.PathEvaluator.Component.IsValid())
+		{
+			bool isEven = (in_rInfo.RingCount & 1) == 0;
+			float magicNumber = -(MATHF_PI * 2 * 0.1f * ((in_rInfo.RingCount >> 1) - isEven + 0.5f));
+
+			float fakeRandom = 1.0f;
+			if (in_rInfo.RingCount > 10)
+			{
+				int tenRingCount = (in_rInfo.RingCount - 1) / 10;
+				fakeRandom = tenRingCount * 0.3 + 0.7f;
+			}
+
+			float scalar{};
+			for (size_t i = 0; i < in_rInfo.RingCount;)
+			{
+				uniqueInfo.Unk1 = { (uniqueInfo.PathEvaluator.GetTangent(uniqueInfo.PathEvaluator.Distance) * in_rInfo.Unk6 * sinf(magicNumber) +
+										-gravityDir * cosf(magicNumber)) * (150.0f * -(0.3f * scalar - fakeRandom)) };
+
+				auto* pObject = new CObjDroppedRing(uniqueInfo);
+				if (!pObject)
+					continue;
+
+				in_rDocument.AddGameObject(pObject);
+				if (++uniqueInfo.Counter >= 3)
+					uniqueInfo.Counter = 0;
+
+				if ((++i % 10) == 0)
+				{
+					magicNumber += MATHF_PI;
+					scalar += 1.0f;
+				}
+
+				magicNumber += MATHF_PI * 2 * 0.1f;
+			}
+
+			return;
+		}
+
+		float random = SonicUSA::System::Random::GetInstance()->genrand_float32();
+		float magicNumber = random * MATHF_PI * 2;
+
+		for (size_t i = 0; i < in_rInfo.RingCount; i++)
+		{
+			csl::math::Vector3 vector{ csl::math::Vector3(sinf(magicNumber), 0.0f, cosf(magicNumber)) * 40.0f * in_rInfo.Unk6 };
+			vector.y() = 100.f;
+
+			uniqueInfo.Unk1 = math::Vector3Rotate(rotation, vector);
+
+			auto* pObject = new CObjDroppedRing(uniqueInfo);
+			if (!pObject)
+				continue;
+
+			in_rDocument.AddGameObject(pObject);
+			if (++uniqueInfo.Counter >= 3)
+				uniqueInfo.Counter = 0;
+
+			magicNumber += ((MATHF_PI * 2) / in_rInfo.RingCount);
+		}
+	}
+}
