df1426595f235a7446b8839787837839320c6b9f
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-17 15:46:46 +0200

    Expand GOCEnemyTarget capabilities

--- Sonic2013/Hedgehog/Game/goc/goc_EnemyTarget.h
+++ Sonic2013/Hedgehog/Game/goc/goc_EnemyTarget.h
@@ -5,23 +5,71 @@ namespace app
 	class GOCEnemyTarget : fnd::GOComponent
 	{
 	public:
-		class Description
+		struct Description
 		{
 		public:
-			bool enabled;
+			bool IsEnabled{};
 		};
 		
 		inline static fnd::GOComponentClass* ms_pGOCEnemyTargetStaticClass = reinterpret_cast<fnd::GOComponentClass*>(ASLR(0x00FE7608));
-		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetup, ASLR(0x00726510), GOCEnemyTarget* This, Description& description);
+		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetup, ASLR(0x00726510), GOCEnemyTarget*, const Description&);
+		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetColliderEyesight, ASLR(0x00726BC0), GOCEnemyTarget*, uint);
+		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetEyesightFrame, ASLR(0x00726310), GOCEnemyTarget*, fnd::HFrame*);
+		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetSphereEyesight, ASLR(0x00726AA0), GOCEnemyTarget*, float);
+		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetEyesightOffset, ASLR(0x00726580), GOCEnemyTarget*, const csl::math::Vector3&);
+		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetEnableEyesight, ASLR(0x007265C0), GOCEnemyTarget*, bool);
+		inline static FUNCTION_PTR(bool, __thiscall, ms_fpIsFindTarget, ASLR(0x00726600), GOCEnemyTarget*);
+		inline static FUNCTION_PTR(void, __thiscall, ms_fpLockTarget, ASLR(0x007266B0), GOCEnemyTarget*);
+		inline static FUNCTION_PTR(csl::math::Vector3*, __thiscall, ms_fpGetTargetCenterPosition, ASLR(0x00726610), GOCEnemyTarget*, csl::math::Vector3*);
 		
 		static fnd::GOComponentClass* staticClass()
 		{
 			return ms_pGOCEnemyTargetStaticClass;
 		}
 
-		void Setup(Description& description)
+		void Setup(const Description& description)
 		{
 			ms_fpSetup(this, description);
+		}
+
+		void SetColliderEyesight(uint in_unk)
+		{
+			ms_fpSetColliderEyesight(this, in_unk);
+		}
+
+		void SetEyesightFrame(fnd::HFrame* in_pFrame)
+		{
+			ms_fpSetEyesightFrame(this, in_pFrame);
+		}
+
+		void SetSphereEyesight(float in_value)
+		{
+			ms_fpSetSphereEyesight(this, in_value);
+		}
+
+		void SetEyesightOffset(const csl::math::Vector3& in_rOffset)
+		{
+			ms_fpSetEyesightOffset(this, in_rOffset);
+		}
+
+		void SetEnableEyesight(bool in_enable)
+		{
+			ms_fpSetEnableEyesight(this, in_enable);
+		}
+
+		bool IsFindTarget()
+		{
+			return ms_fpIsFindTarget(this);
+		}
+
+		void LockTarget()
+		{
+			ms_fpLockTarget(this);
+		}
+
+		csl::math::Vector3* GetTargetCenterPosition(csl::math::Vector3* out_pPosition)
+		{
+			return ms_fpGetTargetCenterPosition(this, out_pPosition);
 		}
 	};
 }
