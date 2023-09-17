958656c38cccf5093f583a809beb615f0281dbb2
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-17 16:04:39 +0200

    Variable initialization changes

--- Sonic2013/Hedgehog/Sound/hhSoundParam.h
+++ Sonic2013/Hedgehog/Sound/hhSoundParam.h
@@ -4,9 +4,9 @@ namespace app::fnd
 {
 	struct SoundParam
 	{
-		float m_Volume{1};
+		float m_Volume{ 1.0f };
 		float m_TweenFactor{}; // Used in a function called MoveToTweenReset
-		size_t m_Unk1;
+		size_t m_Unk1{};
 		SoundDeviceTag m_Device{};
 		hhLong m_StartTime{};
 		bool m_Paused{};
