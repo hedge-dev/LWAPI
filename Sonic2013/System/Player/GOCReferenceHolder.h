e123a6eea0209a607a0d3d512319334f21d1a18d
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-17 16:14:34 +0200

    Expand GOCReferenceHolder capabilities

--- Sonic2013/System/Player/GOCReferenceHolder.h
+++ Sonic2013/System/Player/GOCReferenceHolder.h
@@ -27,6 +27,12 @@ namespace app::Player
 			return fp_GetHolder(this, num);
 		}
 
+		GOCReferenceHolder& GetCurrentHolder()
+		{
+			FUNCTION_PTR(GOCReferenceHolder&, __thiscall, fpGetCurrentHolder, ASLR(0x008F2E60), GOCCollector*);
+			return fpGetCurrentHolder(this);
+		}
+
 		void UpdateChangeRequest()
 		{
 			ms_fpUpdateChangeRequest(this);
@@ -46,7 +52,10 @@ namespace app::Player
 		csl::ut::Bitset<hhUchar, 8> m_Unk1{};
 
 		inline static FUNCTION_PTR(void, __thiscall, ms_fpAddGOCAnimation, ASLR(0x008F41B0), GOCReferenceHolderUnit*, game::GOCAnimationScript*);
+		inline static FUNCTION_PTR(void, __thiscall, ms_fpAddGOCModel, ASLR(0x008F40D0), GOCReferenceHolderUnit*, fnd::GOCVisualModel*);
 		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetOwner, ASLR(0x008F3FA0), GOCReferenceHolderUnit*, GOCReferenceHolder*);
+		inline static FUNCTION_PTR(hh::eff::CInstanceHandle<hh::eff::CEffectInstance>&, __thiscall, ms_fpCreateEffect, ASLR(0x008F3BD0), GOCReferenceHolderUnit*, const game::EffectCreateInfo&);
+		inline static FUNCTION_PTR(hh::eff::CInstanceHandle<hh::eff::CEffectInstance>&, __thiscall, ms_fpCreateEffectFollow, ASLR(0x008F4520), GOCReferenceHolderUnit*, hh::eff::CInstanceHandle<hh::eff::CEffectInstance>*, const game::EffectCreateInfo&);
 
 		void SetOwner(GOCReferenceHolder* in_pOwner)
 		{
@@ -57,10 +66,30 @@ namespace app::Player
 		{
 			ms_fpAddGOCAnimation(this, in_pScript);
 		}
+
+		void AddGOCModel(fnd::GOCVisualModel* in_pModel)
+		{
+			ms_fpAddGOCModel(this, in_pModel);
+		}
+
+		hh::eff::CInstanceHandle<hh::eff::CEffectInstance>& CreateEffect(const game::EffectCreateInfo& in_rCreateInfo)
+		{
+			return ms_fpCreateEffect(this, in_rCreateInfo);
+		}
+
+		hh::eff::CInstanceHandle<hh::eff::CEffectInstance>& CreateEffectFollow(hh::eff::CInstanceHandle<hh::eff::CEffectInstance>* out_pEffect, const game::EffectCreateInfo& in_rCreateInfo)
+		{
+			return ms_fpCreateEffectFollow(this, out_pEffect, in_rCreateInfo);
+		}
 	};
 
 	class GOCReferenceHolder
 	{
+	private:
+		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetup, ASLR(0x008F3460), GOCReferenceHolder*, GameObject*, csl::fnd::IAllocator*);
+		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetupUnitNum, ASLR(0x008F37A0), GOCReferenceHolder*, size_t);
+		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetBlinkVisible, ASLR(0x008F3A50), GOCReferenceHolder*, bool);
+
 	public:
 		csl::ut::InplaceObjectMoveArray<GOCReferenceHolderUnit, 1> m_Units{ nullptr };
 		INSERT_PADDING(8) {};
@@ -77,14 +106,17 @@ namespace app::Player
 
 		void Setup(GameObject* pOwner, csl::fnd::IAllocator* pAlloc)
 		{
-			FUNCTION_PTR(void, __thiscall, fp_Setup, ASLR(0x008F3460), GOCReferenceHolder*, GameObject*, csl::fnd::IAllocator*);
-			fp_Setup(this, pOwner, pAlloc);
+			ms_fpSetup(this, pOwner, pAlloc);
 		}
 
 		void SetupUnitNum(size_t num)
 		{
-			FUNCTION_PTR(void, __thiscall, fp_SetupUnitNum, ASLR(0x008F37A0), GOCReferenceHolder*, size_t);
-			fp_SetupUnitNum(this, num);
+			ms_fpSetupUnitNum(this, num);
+		}
+
+		void SetBlinkVisible(bool in_isVisible)
+		{
+			ms_fpSetBlinkVisible(this, in_isVisible);
 		}
 
 		GOCReferenceHolderUnit& GetUnit(size_t num)
