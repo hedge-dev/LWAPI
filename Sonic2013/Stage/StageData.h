3c365789632392899777f87cfb3340f4e719373d
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-17 16:08:18 +0200

    Expand StageData/Info capabilities

--- Sonic2013/Stage/StageData.h
+++ Sonic2013/Stage/StageData.h
@@ -4,6 +4,7 @@ namespace app::StageInfo
 {
 	class alignas(16) SStageData : public fnd::ReferencedObject
 	{
+	public:
 		enum ClearCondition
 		{
 			CONDITION_GOAL,
@@ -17,6 +18,19 @@ namespace app::StageInfo
 		};
 
 	public:
+		inline static constexpr int STAGE_FLAG_IS_MINIGAME = 0;
+		inline static constexpr int STAGE_FLAG_IS_BATTLE = 1;
+		inline static constexpr int STAGE_FLAG_IS_NO_REDRING = 2;
+		inline static constexpr int STAGE_FLAG_IS_ONLY_NORMALPLAY = 3;
+		inline static constexpr int STAGE_FLAG_IS_SECRET_STAGE = 4;
+		inline static constexpr int STAGE_FLAG_IS_BAKUBAKU_STAGE = 5;
+		inline static constexpr int STAGE_FLAG_IS_DRAW_REFLECT = 6;
+		inline static constexpr int STAGE_FLAG_IS_DISABLE_RANDOMBOX_QUAKE = 7;
+		inline static constexpr int STAGE_FLAG_IS_LASTBOSS = 9;
+		inline static constexpr int STAGE_FLAG_IS_DLC_STAGE = 10;
+		inline static constexpr int STAGE_FLAG_IS_YOSHIISLAND_STAGE = 11;
+		inline static constexpr int STAGE_FLAG_IS_ZELDA_STAGE = 12;
+
 		csl::ut::FixedString<16> m_Name{};
 		csl::ut::FixedString<16> m_Directory{};
 		csl::ut::FixedString<16> m_PathData{};
@@ -36,7 +50,7 @@ namespace app::StageInfo
 		float m_Reverb{};
 		TimeData m_TimeData{};
 		csl::ut::InplaceMoveArray<app::animal::AnimalType, 3> m_Animals{ nullptr };
-		uint m_RequiredAnimals{};
+		int m_RequiredAnimals{};
 		csl::ut::Enum<ClearCondition, char> m_ClearCondition{};
 		float m_EagleAltitude{};
 		
@@ -45,6 +59,98 @@ namespace app::StageInfo
 			ASSERT_OFFSETOF(SStageData, m_PlayerRotations, 112);
 			ASSERT_OFFSETOF(SStageData, m_StageFlags, 140);
 			ASSERT_OFFSETOF(SStageData, m_Phantoms, 144);
+		}
+
+		const char* GetBaseStageName() const
+		{
+			if (IsBattle())
+				return m_Directory.c_str();
+			else
+				return m_Name.c_str();
+		}
+
+		const csl::ut::InplaceMoveArray<app::Game::EPhantomType, app::Game::EPhantomType::PHANTOM_MAX>* GetPhantoms() const
+		{
+			return &m_Phantoms;
+		}
+
+		csl::math::Vector3 GetPlayerPos(int in_playerNo) const
+		{
+			return m_PlayerPositions[in_playerNo];
+		}
+
+		csl::math::Angle3 GetDirection(int in_playerNo) const
+		{
+			return m_PlayerRotations[in_playerNo];
+		}
+
+		bool GetReverb(float* out_pReverb) const
+		{
+			if (m_Reverb < 0.0f)
+				return false;
+		
+			*out_pReverb = m_Reverb;
+			return true;
+		}
+
+		bool IsMinigame() const
+		{
+			return m_StageFlags.test(STAGE_FLAG_IS_MINIGAME);
+		}
+
+		bool IsBattle() const
+		{
+			return m_StageFlags.test(STAGE_FLAG_IS_BATTLE);
+		}
+
+		bool IsNoRedRing() const
+		{
+			return m_StageFlags.test(STAGE_FLAG_IS_NO_REDRING);
+		}
+
+		bool IsOnlyNormalPlay() const
+		{
+			return m_StageFlags.test(STAGE_FLAG_IS_ONLY_NORMALPLAY);
+		}
+
+		bool IsSecretStage() const
+		{
+			return m_StageFlags.test(STAGE_FLAG_IS_SECRET_STAGE);
+		}
+
+		bool IsBakuBakuStage() const
+		{
+			return m_StageFlags.test(STAGE_FLAG_IS_BAKUBAKU_STAGE);
+		}
+
+		bool IsDrawReflect() const
+		{
+			return m_StageFlags.test(STAGE_FLAG_IS_DRAW_REFLECT);
+		}
+
+		bool IsDisableRandomBoxQuake() const
+		{
+			return m_StageFlags.test(STAGE_FLAG_IS_DISABLE_RANDOMBOX_QUAKE);
+		}
+
+		bool IsLastBossStage() const
+		{
+			return m_StageFlags.test(STAGE_FLAG_IS_LASTBOSS);
+		}
+
+		bool IsDLCStage() const
+		{
+			return m_StageFlags.test(STAGE_FLAG_IS_DLC_STAGE);
+		}
+
+		bool IsYoshiIslandStage() const
+		{
+			return m_StageFlags.test(STAGE_FLAG_IS_YOSHIISLAND_STAGE);
+		}
+
+		bool IsZeldaStage() const
+		{
+			return m_StageFlags.test(STAGE_FLAG_IS_ZELDA_STAGE);
 		}
 	};
 }
--- Sonic2013/Stage/StageInfo.h
+++ Sonic2013/Stage/StageInfo.h
@@ -2,6 +2,33 @@
 
 namespace app::StageInfo
 {
+	static std::pair<const char*, Game::EPhantomType> phantomMap[Game::EPhantomType::PHANTOM_MAX] =
+	{
+		{ "drill",		Game::EPhantomType::PHANTOM_DRILL },
+		{ "laser",		Game::EPhantomType::PHANTOM_LASER },
+		{ "rocket",		Game::EPhantomType::PHANTOM_ROCKET },
+		{ "eagle",		Game::EPhantomType::PHANTOM_EAGLE },
+		{ "asteroid",	Game::EPhantomType::PHANTOM_ASTEROID },
+		{ "rhythm",		Game::EPhantomType::PHANTOM_RHYTHM },
+		{ "bomb",		Game::EPhantomType::PHANTOM_BOMB },
+		{ "hover",		Game::EPhantomType::PHANTOM_HOVER },
+		{ "quake",		Game::EPhantomType::PHANTOM_QUAKE }
+	};
+	
+	static std::pair<const char*, animal::AnimalType> animalMap[animal::AnimalType::ANIMAL_MAX] =
+	{
+		{ "FLICKIE",	animal::AnimalType::ANIMAL_FLICKIE },
+		{ "COOKIE",		animal::AnimalType::ANIMAL_COOKIE },
+		{ "PECKIE",		animal::AnimalType::ANIMAL_PECKIE },
+		{ "PICKIE",		animal::AnimalType::ANIMAL_PICKIE },
+		{ "POCKY",		animal::AnimalType::ANIMAL_POCKY },
+		{ "RICKY",		animal::AnimalType::ANIMAL_RICKY },
+		{ "ROCKY",		animal::AnimalType::ANIMAL_ROCKY },
+	};
+
+	static Game::EPhantomType PhantomTypeFromName(const char* in_pName);
+	static animal::AnimalType AnimalTypeFromName(const char* in_pName);
+
 	class CStageInfo
 	{
 	public:
@@ -35,6 +62,182 @@ namespace app::StageInfo
 			}
 
 			return nullptr;
+		}
+
+		static void ReadMission_Mission(const char* in_pCat, game::LuaScript& in_rScript, void* in_pCtx)
+		{
+			auto* pStageData = new(static_cast<CStageInfo*>(in_pCtx)->m_pAllocator) StageInfo::SStageData();
+			const char* pString{};
+
+			if (!in_rScript.GetString("name", pString))
+				return;
+		
+			pStageData->m_Name = pString;
+
+			if (in_rScript.GetString("dir", pString))
+				pStageData->m_Directory = pString;
+			else
+				pStageData->m_Directory = pStageData->m_Name;
+
+			if (in_rScript.GetString("path_data", pString))
+				pStageData->m_PathData = pString;
+			else
+				pStageData->m_PathData = pStageData->m_Directory;
+		
+			if (!in_rScript.GetVector3("player_pos", pStageData->m_PlayerPositions[0]))
+				pStageData->m_PlayerPositions[0] = { 0.0f, 0.0f, 0.0f };
+
+			csl::math::Vector3 direction{};
+			if (in_rScript.GetVector3("player_dir", direction))
+			{
+				pStageData->m_PlayerRotations[0].x() = direction.x() * MATHF_PI / 180.0f;
+				pStageData->m_PlayerRotations[0].y() = direction.y() * MATHF_PI / 180.0f;
+				pStageData->m_PlayerRotations[0].z() = direction.z() * MATHF_PI / 180.0f;
+			}
+
+			if (!in_rScript.GetVector3("player2_pos", pStageData->m_PlayerPositions[1]))
+			{
+				pStageData->m_PlayerPositions[1] = pStageData->m_PlayerPositions[0];
+			}
+
+			if (in_rScript.GetVector3("player2_dir", direction))
+			{
+				pStageData->m_PlayerRotations[1].x() = direction.x() * MATHF_PI / 180.0f;
+				pStageData->m_PlayerRotations[1].y() = direction.y() * MATHF_PI / 180.0f;
+				pStageData->m_PlayerRotations[1].z() = direction.z() * MATHF_PI / 180.0f;
+			}
+			else
+			{
+				pStageData->m_PlayerRotations[1] = pStageData->m_PlayerRotations[0];
+			}
+
+			if (in_rScript.GetString("bgm", pString))
+				pStageData->m_Bgm = pString;
+
+			if (in_rScript.GetString("bgm_sv", pString))
+				pStageData->m_BgmSv = pString;
+
+			if (in_rScript.GetString("amb", pString))
+				pStageData->m_Amb = pString;
+
+			if (in_rScript.GetString("amb_in", pString))
+				pStageData->m_AmbIn = pString;
+
+			in_rScript.GetFloat("reverb", pStageData->m_Reverb);
+
+			if (in_rScript.GetString("player_mode", pString))
+			{
+				if (!strcmp(pString, "2D"))
+					pStageData->m_PlayerFlags.set(0);
+				else if (!strcmp(pString, "WATER"))
+					pStageData->m_PlayerFlags.set(1);
+				else if (!strcmp(pString, "SKYCHASE"))
+					pStageData->m_PlayerFlags.set(2);
+				else if (!strcmp(pString, "TOP"))
+					pStageData->m_PlayerFlags.set(3);
+				else if (!strcmp(pString, "SNOWBALL"))
+					pStageData->m_PlayerFlags.set(4);
+			}
+			
+			ut::InplaceTempArray<const char*, 10> stageAttrs{};
+			if (in_rScript.GetStringArray("attrs", stageAttrs))
+			{
+				for (size_t i = 0; i < stageAttrs.size(); i++)
+				{
+					if (!strcmp(stageAttrs[i], "no_redring"))
+						pStageData->m_StageFlags.set(2, true);
+
+					if (!strcmp(stageAttrs[i], "only_normalplay"))
+					{
+						pStageData->m_StageFlags.set(3, true);
+						pStageData->m_StageFlags.set(4, true);
+					}
+
+					if (!strcmp(stageAttrs[i], "bakubaku"))
+						pStageData->m_StageFlags.set(5, true);
+
+					if (!strcmp(stageAttrs[i], "floor_reflect"))
+						pStageData->m_StageFlags.set(6, true);
+
+					if (!strcmp(stageAttrs[i], "randombox_noquake"))
+						pStageData->m_StageFlags.set(7, true);
+
+					if (!strcmp(stageAttrs[i], "no_bomb"))
+						pStageData->m_StageFlags.set(8, true);
+
+					if (!strcmp(stageAttrs[i], "lastboss"))
+						pStageData->m_StageFlags.set(9, true);
+
+					if (!strcmp(stageAttrs[i], "dlc"))
+					{
+						pStageData->m_StageFlags.set(3, true);
+						pStageData->m_StageFlags.set(10, true);
+					}
+
+					if (!strcmp(stageAttrs[i], "yoshiisland"))
+						pStageData->m_StageFlags.set(11, true);
+
+					if (!strcmp(stageAttrs[i], "zelda"))
+						pStageData->m_StageFlags.set(12, true);
+				}
+			}
+
+			if (strstr(pStageData->m_Name.c_str(), "mini"))
+				pStageData->m_StageFlags.set(0, true);
+			else if (strstr(pStageData->m_Name.c_str(), "batl"))
+				pStageData->m_StageFlags.set(1, true);
+
+			ut::InplaceTempArray<const char*, Game::EPhantomType::PHANTOM_MAX> phantoms{};
+			if (in_rScript.GetStringArray("phantoms", phantoms))
+			{
+				for (size_t i = 0; i < phantoms.size(); i++)
+				{
+					Game::EPhantomType type = PhantomTypeFromName(phantoms[i]);
+					if (type != static_cast<Game::EPhantomType>(-1))
+						pStageData->m_Phantoms.push_back_unchecked(type);
+				}
+			}
+
+			if (pStageData->m_StageFlags.test(1))
+				pStageData->m_Phantoms.push_back_unchecked(Game::EPhantomType::PHANTOM_QUAKE);
+			else if (!pStageData->m_StageFlags.test(0) && !pStageData->m_StageFlags.test(1) && !pStageData->m_StageFlags.test(8))
+				pStageData->m_Phantoms.push_back_unchecked(Game::EPhantomType::PHANTOM_BOMB);
+
+			if (in_rScript.OpenNode("time"))
+			{
+				in_rScript.GetFloat("time_limit", pStageData->m_TimeData.m_Limit);
+				ut::InplaceTempArray<float, 4> times{};
+				if (in_rScript.GetFloatArray("rank", times))
+				{
+					for (size_t i = 0; i < times.size(); i++)
+						pStageData->m_TimeData.m_RankIntervals[i] = times[i];
+				}
+
+				in_rScript.CloseNode();
+			}
+
+			ut::InplaceTempArray<const char*, 3> animals{};
+			if (in_rScript.GetStringArray("animals", animals))
+			{
+				for (size_t i = 0; i < animals.size(); i++)
+					pStageData->m_Animals.push_back_unchecked(AnimalTypeFromName(animals[i]));
+			}
+
+			if (!in_rScript.GetInt("unlock_animals", pStageData->m_RequiredAnimals))
+				pStageData->m_RequiredAnimals = 0;
+
+			if (in_rScript.GetString("clear_cond", pString))
+			{
+				if (!strcmp(pString, "goal"))
+					pStageData->m_ClearCondition = StageInfo::SStageData::ClearCondition::CONDITION_GOAL;
+				else if (!strcmp(pString, "timeout"))
+					pStageData->m_ClearCondition = StageInfo::SStageData::ClearCondition::CONDITION_TIMEOUT;
+			}
+
+			if (!in_rScript.GetFloat("eagle_altitude", pStageData->m_EagleAltitude))
+				pStageData->m_EagleAltitude = 20.0f;
+
+			static_cast<CStageInfo*>(in_pCtx)->m_Stages.push_back(pStageData);
 		}
 
 		void ReadCategoryDebug(const char* pCat, game::LuaScript& rScript, size_t worldIdx)
@@ -72,4 +275,26 @@ namespace app::StageInfo
 				}, &m_Worlds[worldIdx]);
 		}
 	};
+
+	Game::EPhantomType PhantomTypeFromName(const char* in_pName)
+	{
+		for (size_t i = 0; i < Game::EPhantomType::PHANTOM_MAX; i++)
+		{
+			if (!strcmp(in_pName, phantomMap[i].first))
+				return phantomMap[i].second;
+		}
+
+		return static_cast<Game::EPhantomType>(-1);
+	}
+
+	animal::AnimalType AnimalTypeFromName(const char* in_pName)
+	{
+		for (size_t i = 0; i < animal::AnimalType::ANIMAL_MAX; i++)
+		{
+			if (!strcmp(in_pName, animalMap[i].first))
+				return animalMap[i].second;
+		}
+
+		return static_cast<animal::AnimalType>(animal::AnimalType::ANIMAL_MAX + 1);
+	}
 }
