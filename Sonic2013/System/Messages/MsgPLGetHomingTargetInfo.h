f43996acac80a9f5f015f7c7cd7408f41e4e0153
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-17 16:11:16 +0200

    Update mappings

--- Sonic2013/System/Messages/MsgPLGetHomingTargetInfo.h
+++ Sonic2013/System/Messages/MsgPLGetHomingTargetInfo.h
@@ -5,19 +5,19 @@ namespace app::xgame
 	class MsgPLGetHomingTargetInfo : public fnd::Message
 	{
 	public:
+		csl::ut::Bitset<uint> m_Flags{};
+		csl::math::Vector3 m_CursorPosition{};
+		GameObject* pUnk1{};
+		size_t TargetShapeID{};
+		size_t m_LockonCount{ 1 };
+		int Unk2{};
+		const game::ColliShape* pTargetShape{};
+		
 		inline const static unsigned int MessageID = 0x5004;
 		
-		csl::ut::Bitset<uint> m_Flags{};
-		csl::math::Vector3 m_CursorPosition{ 0, 0, 0 };
-		INSERT_PADDING(8){};
-		size_t m_LockonCount{ 1 };
-		void* m_Unk1{};
-		game::ColliShape* m_pShape{ nullptr };
-		
-		MsgPLGetHomingTargetInfo() : Message(MessageID)
+		MsgPLGetHomingTargetInfo(const game::ColliShape* in_pTargetShape) : Message(MessageID), pTargetShape(in_pTargetShape), TargetShapeID(in_pTargetShape->m_ID)
 		{
-			ASSERT_OFFSETOF(MsgPLGetHomingTargetInfo, m_LockonCount, 56);
-			ASSERT_OFFSETOF(MsgPLGetHomingTargetInfo, m_pShape, 64);
+			
 		}
 
 		Message* Clone() override
