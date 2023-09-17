a8d0faa8d38b336115c90bdeeb943df97c244631
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-17 15:55:07 +0200

    Expand GOCVisual's capabilities

--- Sonic2013/Hedgehog/Graphics/goc/goc_VisualModel.h
+++ Sonic2013/Hedgehog/Graphics/goc/goc_VisualModel.h
@@ -13,10 +13,20 @@ namespace app::fnd
 	struct TexSrtDesc;
 	class MatAnimBlenderHH;
 	class TexSrtBlenderHH;
+	class TexSrtControlHH;
+	class TexSrtControl;
 
 	class GOCVisualModel : public GOCVisualTransformed
 	{
 	public:
+		enum class LightQualityType : signed char
+		{
+			LightQualityType_VALUE0,
+			LightQualityType_VALUE1,
+			LightQualityType_VALUE2,
+			LightQualityType_MAX
+		};
+
 		typedef uint NodeSpace;
 
 		class Description
@@ -26,7 +36,7 @@ namespace app::fnd
 			hh::gfx::res::ResSkeleton m_Skeleton{};
 			int field_08{}; // some kind of flags
 			uint field_0C{};
-			int field_10{}; // light quality type?
+			csl::ut::Enum<LightQualityType, signed char> m_LightQualityType{ LightQualityType::LightQualityType_MAX };
 			HFrame* m_pParent{}; // parent?
 			float zOffset{};
 			void* field_1C{}; // GI texture?
@@ -47,11 +57,14 @@ namespace app::fnd
 		inline static FUNCTION_PTR(bool, __thiscall, ms_fpSetup, ASLR(0x00495850), void* This, const Description&);
 		inline static FUNCTION_PTR(TexSrtBlenderHH*, __thiscall, ms_fpSetTexSrtBlender, ASLR(0x00495930), void*, const TexSrtBlenderDesc&);
 		inline static FUNCTION_PTR(MatAnimBlenderHH*, __thiscall, ms_fpSetMatAnimBlender, ASLR(0x00495A70), void*, const MatAnimBlenderDesc&);
+		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetMaterialColor, ASLR(0x00495140), void*, const float*);
 		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetMaterialAnimation, ASLR(0x004959D0), void*, const MatAnimDesc&);
-		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetTexSrtAnimation, ASLR(0x00495890), void*, const TexSrtDesc&);
+		inline static FUNCTION_PTR(TexSrtControl*, __thiscall, ms_fpSetTexSrtAnimation, ASLR(0x00495890), void*, const TexSrtDesc&);
+		inline static FUNCTION_PTR(TexSrtControl*, __thiscall, ms_fpGetTexSrtControl, ASLR(0x004954C0), const GOCVisualModel*);
 		inline static FUNCTION_PTR(void, __thiscall, ms_fpAttachAnimation, ASLR(0x004951B0), void*, game::GOCAnimationSingle*);
-		inline static FUNCTION_PTR(void, __thiscall, ms_fpGetNodeTransform, ASLR(0x004951F0), const void*, NodeSpace, const char*, csl::math::Transform*);
-		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetNodeTransform, ASLR(0x00495240), void*, NodeSpace, const char*, const csl::math::Transform&);
+		inline static FUNCTION_PTR(bool, __thiscall, ms_fpGetNodeTransform, ASLR(0x004951F0), const void*, NodeSpace, const char*, app::math::Transform*);
+		inline static FUNCTION_PTR(bool, __thiscall, ms_fpGetNodeTransformByIndex, ASLR(0x004951D0), const void*, NodeSpace, int, app::math::Transform*);
+		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetNodeTransform, ASLR(0x00495240), void*, NodeSpace, const char*, const app::math::Transform&);
 
 		static GOComponentClass* staticClass()
 		{
@@ -93,14 +106,24 @@ namespace app::fnd
 			return ms_fpSetMatAnimBlender(this, in_description);
 		}
 
+		void SetMaterialColor(const float* in_pColor)
+		{
+			ms_fpSetMaterialColor(this, in_pColor);
+		}
+
 		void SetMaterialAnimation(const MatAnimDesc& in_description)
 		{
 			ms_fpSetMaterialAnimation(this, in_description);
 		}
 
-		void SetTexSrtAnimation(const TexSrtDesc& in_description)
+		TexSrtControl* SetTexSrtAnimation(const TexSrtDesc& in_description)
 		{
-			ms_fpSetTexSrtAnimation(this, in_description);
+			return ms_fpSetTexSrtAnimation(this, in_description);
+		}
+
+		TexSrtControl* GetTexSrtControl() const
+		{
+			return ms_fpGetTexSrtControl(this);
 		}
 
 		void AttachAnimation(game::GOCAnimationSingle* in_pAnimation)
@@ -108,12 +131,17 @@ namespace app::fnd
 			ms_fpAttachAnimation(this, in_pAnimation);
 		}
 
-		void GetNodeTransform(NodeSpace in_nodeSpace, const char* in_pNodeName, csl::math::Transform* in_pTransform) const
+		bool GetNodeTransform(NodeSpace in_nodeSpace, const char* in_pNodeName, app::math::Transform* out_pTransform) const
 		{
-			ms_fpGetNodeTransform(this, in_nodeSpace, in_pNodeName, in_pTransform);
+			return ms_fpGetNodeTransform(this, in_nodeSpace, in_pNodeName, out_pTransform);
 		}
 
-		void SetNodeTransform(NodeSpace in_nodeSpace, const char* in_pNodeName, const csl::math::Transform& in_transform)
+		bool GetNodeTransform(NodeSpace in_nodeSpace, int in_nodeIndex, app::math::Transform* out_pTransform) const
+		{
+			return ms_fpGetNodeTransformByIndex(this, in_nodeSpace, in_nodeIndex, out_pTransform);
+		}
+
+		void SetNodeTransform(NodeSpace in_nodeSpace, const char* in_pNodeName, const app::math::Transform& in_transform)
 		{
 			ms_fpSetNodeTransform(this, in_nodeSpace, in_pNodeName, in_transform);
 		}
@@ -141,9 +169,19 @@ namespace app::fnd
 		size_t m_Unk;
 	};
 
+	class TexSrtControl
+	{
+	public:
+		inline static FUNCTION_PTR(void, __thiscall, ms_fpSetSpeed, ASLR(0x00496D40), TexSrtControl*, float);
+
+		void SetSpeed(float in_speed)
+		{
+			ms_fpSetSpeed(this, in_speed);
+		}
+	};
+
 	class TexSrtBlenderHH
 	{
-		class TexSrtControlHH;
 		typedef TexSrtDesc TexSrtControlDesc;
 
 	public:
