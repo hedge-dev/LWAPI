ef9c576144ede1acf1b135fcfca2408822b311dd
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-17 16:32:29 +0200

    Add objects used by Gamemodes to function

---
+++ Sonic2013/Object/Stage/ObjColorParamChanger.h
@@ -0,0 +1,51 @@
+#pragma once
+
+namespace app
+{
+	class ObjColorParamChanger : public GameObject
+	{
+		enum State : byte
+		{
+
+		};
+
+		csl::ut::Enum<State, byte> CurrentState{};
+		char Unk1{};
+		char Unk2{};
+		float Unk3{};
+
+
+	private:
+		inline static FUNCTION_PTR(ObjColorParamChanger*, __thiscall, ms_fpCtor, ASLR(0x0075BDF0), ObjColorParamChanger*);
+		inline static FUNCTION_PTR(ObjColorParamChanger*, __thiscall, ms_fpDtor, ASLR(0x0075BE30), ObjColorParamChanger*);
+		inline static FUNCTION_PTR(void, __thiscall, ms_fpRestart, ASLR(0x0075BF50), ObjColorParamChanger*);
+
+	public:
+		ObjColorParamChanger()
+		{
+			ms_fpCtor(this);
+		}
+
+		~ObjColorParamChanger() override
+		{
+			ms_fpDtor(this);
+		}
+
+		void Restart()
+		{
+			ms_fpRestart(this);
+		}
+
+		// Temporary, does not actually exist
+		static ObjColorParamChanger* Create(csl::fnd::IAllocator* in_pAllocator)
+		{
+			void* mem = in_pAllocator->Alloc(sizeof(ObjColorParamChanger), 16);
+			return ms_fpCtor(static_cast<ObjColorParamChanger*>(mem));
+		}
+
+		/*ObjColorParamChanger() : GameObject()
+		{
+			SetObjectCategory(13);
+		}*/
+	};
+}
