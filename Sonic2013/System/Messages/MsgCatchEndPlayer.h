a91955b323eef91993137d38f85b299f1911acd6
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-17 16:09:56 +0200

    Update mapping of Catch Player message types

--- Sonic2013/System/Messages/MsgCatchEndPlayer.h
+++ Sonic2013/System/Messages/MsgCatchEndPlayer.h
@@ -5,7 +5,7 @@ namespace app::xgame
 	class MsgCatchEndPlayer : public fnd::Message
 	{
 	public:
-		INSERT_PADDING(4);
+		bool Unk1{};
 
 		inline const static unsigned int MessageID = 0x601D;
 		
--- Sonic2013/System/Messages/MsgCatchPlayer.h
+++ Sonic2013/System/Messages/MsgCatchPlayer.h
@@ -6,16 +6,15 @@ namespace app::xgame
 	{
 	public:
 		csl::ut::Bitset<char> m_Unk1{};
-		INSERT_PADDING(4);
 		csl::math::Matrix34 m_Unk2{};
 		int m_Unk3{};
-		INSERT_PADDING(12);
+		bool m_Unk4{};
 
 		inline const static unsigned int MessageID = 0x601C;
 		
 		MsgCatchPlayer() : Message(MessageID)
 		{
-			
+
 		}
 
 		Message* Clone() override
