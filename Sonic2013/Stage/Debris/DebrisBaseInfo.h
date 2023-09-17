dc6a4242334a2982ade47c2bd451c3796c9ad9b5
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-17 16:21:39 +0200

    Object debris related expansion

---
+++ Sonic2013/Stage/Debris/DebrisBaseInfo.h
@@ -0,0 +1,33 @@
+#pragma once
+
+namespace app::debris
+{
+	typedef int EInfoType;
+
+	struct SDebrisBaseInfo
+	{
+		math::Transform Transform{};
+		float Unk1{};
+		float Unk2{};
+		float Unk3{};
+		csl::ut::Bitset<uint> Unk4{};
+		csl::math::Vector3 Unk5{};
+		csl::math::Vector3 Unk6{};
+		float Unk7{};
+		float Unk8{};
+		INSERT_PADDING(8) {};
+		csl::math::Vector3 field_80{};
+		float Unk9{};
+		INSERT_PADDING(12) {};
+		float Unk10{};
+		float Unk11{};
+		int Unk12{ 176 };
+		int Unk13{};
+		EInfoType Type{};
+
+		SDebrisBaseInfo(EInfoType in_infoType)
+		{
+			Type = in_infoType;
+		}
+	};
+}
---
+++ Sonic2013/Stage/Debris/Resource/ResRandomSpaceDebris.h
@@ -0,0 +1,37 @@
+#pragma once
+
+namespace app::debris
+{
+	class ResRandomSpaceDebris
+	{
+	public:
+		struct SDesc
+		{
+			const char* pModelName{};
+			hh::ut::PackFile* pPackFile{};
+		};
+
+		csl::ut::FixedArray<hh::gfx::res::ResModel, 16> Models{};
+		int ModelCount{};
+
+	public:
+		ResRandomSpaceDebris()
+		{
+
+		}
+
+		void Build(SDesc& in_rDescription)
+		{
+			Models.assign({});
+			for (size_t i = 0; i < Models.size(); i++)
+			{
+				char buffer[64]{};
+				csl::fnd::Snprintf(buffer, sizeof(buffer), "%s%02d", in_rDescription.pModelName, i + 1);
+
+				Models[i] = ObjUtil::GetModelResource(buffer, *in_rDescription.pPackFile);
+				if (Models[i].IsValid())
+					ModelCount = i + 1;
+			}
+		}
+	};
+}
