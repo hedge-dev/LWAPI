57ee2c839a5ec31ac5c95776480f3c830c5c966b
Sajid <sajidur78@gmail.com>
2023-10-21 03:05:50 +0600

    Add CSampleChunkResource2 Resolve & Unresolve pointer

--- Sonic2013/Hedgehog/Database/Resource/hhSampleChunkResource.h
+++ Sonic2013/Hedgehog/Database/Resource/hhSampleChunkResource.h
@@ -8,9 +8,40 @@ namespace hh::db
 		inline static FUNCTION_PTR(void, __thiscall, ms_fpResolvePointer, ASLR(0x00C27CB0), CSampleChunkResource2*);
 
 	public:
+		uint32_t m_Size;
+		uint32_t m_Signature;
+		uint32_t m_OffsetTablePtr;
+		uint32_t m_OffsetCount;
+
+		void* GetData()
+		{
+			return reinterpret_cast<void*>(this + 1);
+		}
+
 		void ResolvePointer()
 		{
-			ms_fpResolvePointer(this);
+			uint32_t* offsets = reinterpret_cast<uint32_t*>((char*)this + _byteswap_ulong(m_OffsetTablePtr));
+			auto* data = (char*)GetData();
+
+			for (size_t i = 0; i < _byteswap_ulong(m_OffsetCount); i++)
+			{
+				auto offset = _byteswap_ulong(offsets[i]);
+				auto* ptr = data + offset;
+				*(size_t*)ptr = (size_t)ptr;
+			}
+		}
+
+		void UnResolvePointer()
+		{
+			uint32_t* offsets = reinterpret_cast<uint32_t*>((char*)this + _byteswap_ulong(m_OffsetTablePtr));
+			auto* data = (char*)GetData();
+
+			for (size_t i = 0; i < _byteswap_ulong(m_OffsetCount); i++)
+			{
+				auto offset = _byteswap_ulong(offsets[i]);
+				auto* ptr = data + offset;
+				*(size_t*)ptr = _byteswap_ulong((size_t)ptr); // Re-save in flipped endianness
+			}
 		}
 	};
 }
