76eb87102963e9144a6c2f0082068b9346cb2d7f
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-11-03 20:36:01 +0100

    Further corrections to Hedgehog/Extra

--- Sonic2013/Hedgehog/Extra/Schedule/hhFxPipelineExecutor.h
+++ Sonic2013/Hedgehog/Extra/Schedule/hhFxPipelineExecutor.h
@@ -8,17 +8,17 @@ namespace Extra::fx
 		class CFxPipelineExecutorInternal
 		{
 		public:
-			inline static FUNCTION_PTR(void, __cdecl, ms_fpRebuildTextureSurface, ASLR(0x00C50C40), hh::rsdx::RsdxSurface9* in_surface, Extra::fx::STextureSurfacePair& out_pair, bool a3);
+			inline static FUNCTION_PTR(void, __cdecl, ms_fpRebuildTextureSurface, ASLR(0x00C50C40), hh::rsdx::RsdxSurface9*, Extra::fx::STextureSurfacePair&, bool);
 
-			static void RebuildTextureSurface(hh::rsdx::RsdxSurface9* in_surface, Extra::fx::STextureSurfacePair& out_pair, bool a3)
+			static void RebuildTextureSurface(hh::rsdx::RsdxSurface9* in_pSurface, Extra::fx::STextureSurfacePair& out_rPair, bool in_a3)
 			{
-				ms_fpRebuildTextureSurface(in_surface, out_pair, a3);
+				ms_fpRebuildTextureSurface(in_pSurface, out_rPair, in_a3);
 			}
 		};
 
-		static void RebuildTextureSurface(hh::rsdx::RsdxSurface9* in_surface, Extra::fx::STextureSurfacePair& out_pair, bool a3)
+		static void RebuildTextureSurface(hh::rsdx::RsdxSurface9* in_pSurface, Extra::fx::STextureSurfacePair& out_rPair, bool in_a3)
 		{
-			CFxPipelineExecutorInternal::RebuildTextureSurface(in_surface, out_pair, a3);
+			CFxPipelineExecutorInternal::RebuildTextureSurface(in_pSurface, out_rPair, in_a3);
 		}
 	};
 }
