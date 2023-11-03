8241cb56b572c0becef87408ff7201402797b2ef
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-11-03 23:33:48 +0100

    Further corrections to Sonicteam

--- Sonic2013/SonicUSA/System/RadianMask.h
+++ Sonic2013/SonicUSA/System/RadianMask.h
@@ -2,16 +2,15 @@
 
 namespace SonicUSA::System
 {
-	inline static FUNCTION_PTR(float, __cdecl, ms_fpRadianMaskU, ASLR(0x0097B280), float);
-	inline static FUNCTION_PTR(float, __cdecl, ms_fpRadianMaskS, ASLR(0x0097B2E0), float);
-
 	inline float RadianMaskU(float in_value)
 	{
-		return ms_fpRadianMaskU(in_value);
+		FUNCTION_PTR(float, __cdecl, fpRadianMaskU, ASLR(0x0097B280), float);
+		return fpRadianMaskU(in_value);
 	}
 
 	inline float RadianMaskS(float in_value)
 	{
-		return ms_fpRadianMaskS(in_value);
+		FUNCTION_PTR(float, __cdecl, fpRadianMaskS, ASLR(0x0097B2E0), float);
+		return fpRadianMaskS(in_value);
 	}
 }
